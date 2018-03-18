9461a9be318a857bd1f9f4177df69d32ee39c24f
Tony Twyman <nw2056@gmail.com>
2018-03-17 20:41:48 -0700

    Tokenizer.class.php: - Added regex array which is used while searching for Token matches - Removed all switch statements and replaced them with if statements using regex
    General:
    - Updated and added comments that are more inline with phpDOC style guide

--- Compiler/Parser.class.php
+++ Compiler/Parser.class.php
@@ -4,18 +4,31 @@
     /**
      * All Token Types that will be used by both the Parser and Tokenizer
      */
+     /** @var int T_BRACES_OPEN   */
      const T_BRACES_OPEN        = 1;
+     /** @var int T_BRACES_CLOSE */
      const T_BRACES_CLOSE       = 2;
+     /** @var int T_FUNCTION_START */
      const T_FUNCTION_START     = 3;
+     /** @var int T_FUNCTION_END */
      const T_FUNCTION_END       = 4;
+     /** @var int T_IF */
      const T_IF                 = 5;
+     /** @var int T_ELSE */
      const T_ELSE               = 6;
+     /** @var int T_EACH */
      const T_EACH               = 7;
+     /** @var int T_FIRST */
      const T_FIRST              = 8;
+     /** @var int T_LAST */
      const T_LAST               = 9;
+     /** @var int T_AT */
      const T_AT                 = 10;
+     /** @var int T_ALPHA */
      const T_ALPHA              = 11;
+     /** @var int T_NUMERIC */
      const T_NUMERIC            = 12;
+     /** @var int T_VARIABLE */
      const T_VARIABLE           = 13;
 
   }
--- Compiler/Tokenizer.class.php
+++ Compiler/Tokenizer.class.php
@@ -3,9 +3,8 @@
   class Tokenizer {
 
     /**
-     * All operator strings that will be searched for while tokenizing the input
      * @static
-     * @var array
+     * @var array $operators All operator strings that will be searched for while tokenizing the input
      */
     private static $operators = array(
       '{{'        => Parser::T_BRACES_OPEN,
@@ -15,9 +14,8 @@
     );
 
     /**
-     * All keyword strings that will be searched for while tokenizing the input
      * @static
-     * @var array
+     * @var array $keywords All keyword strings that will be searched for while tokenizing the input
      */
     private static $keywords = array(
       'unless'     => Parser::T_IF,
@@ -28,75 +26,89 @@
     );
 
     /**
+     * @static
+     * @var array $regex All regular expressions that will be used in tokenizer
+     */
+    private static $regex = array(
+      '/[0-9]/',
+      '/[\{\}\#\@]/',
+      '/[a-zA-Z]/'
+    );
+
+    /**
+     * All constants that correspond to a specific regex
+     */
+
+    /** @var int RE_NUMERIC */
+    const RE_NUMERIC       = 0;
+    /** @var int RE_OPERATORS */
+    const RE_OPERATORS     = 1;
+    /** @var int RE_ALPHA */
+    const RE_ALPHA         = 2;
+
+    /**
      * Creates an array of tokens from the input line
-     * @param string the string that will be tokenized
+     * @param string $line The string that will be tokenized
      */
     public function tokenize(string $line) {
-      /**
-       * Bool that determines if we are looking for keywords, or strings
-       * Will be set true if the Tokenizer finds opening function braces: '{{'
-       * @var bool
-       */
+      /** @var bool $search_keywords Determines if we are looking for keywords or strings */
       $search_keywords = false;
-
-      /** @var int */
+      /** @var int $offset Current position in the input line */
       $offset = 0;
-
-      /** @var int */
+      /** @var int $len Length of input line*/
       $len = strlen($line);
-
-      /**
-       * An array of tokens that will be used by the parser
-       * @var array
-       */
+      /** @var array $tokens An array of tokens that will be used by the parser */
       $tokens = array();
 
       for($i = 0; $i < $len; $i++) {
-        $offset = $i;
-
-        /** @var Token */
+        /** @var Token $token An empty object that will be used to add new token to token array */
         $token = NULL;
-
-        switch($line[$i]) {
-          case '.':
-            break;
-          //Check if
-          case '0': case '1': case '2': case '3': case '4':
-          case '5': case '6': case '7': case '8': case '9':
-            $token = self::tokenizeNumeric($line, $i, $len);
-            array_push($tokens, $token);
-            break;
-          case '{': case '}': case '#': case '@':
-            foreach(self::$operators as $value => $type) {
+        $offset = $i;
+        // Use $regex to search for a match on the current character in $line
+        if(preg_match(self::$regex[self::RE_NUMERIC], $line[$i])) {
+          $token = self::tokenizeNumeric($line, $i, $len);
+          array_push($tokens, $token);
+        } else if(preg_match(self::$regex[self::RE_OPERATORS], $line[$i])) {
+          /**
+           * Compare each operator in $operators to the current character in $line
+           * @var string $value The operator string
+           * @var string $type The operator type from Parser
+           */
+          foreach(self::$operators as $value => $type) {
+            /** @var int $op_len Length of the operator we are comparing */
+            $op_len = strlen($value);
+            if(substr_compare($line, $value, $i, $op_len) == 0){
+              $token = new Token($type, $value, $i);
+              array_push($tokens, $token);
+              $i += $op_len - 1;
+              /** If we have found the opening or close funtion braces, update $search_keywords */
+              if($value == '{{') {
+                $search_keywords = true;
+              } else if($value == '}}'){
+                $search_keywords = false;
+              }
+            }
+          }
+        } else if(preg_match(self::$regex[self::RE_ALPHA], $line[$i])) {
+          if($search_keywords){
+            /**
+             * Compare each keyword in $keywords to the current character in $line
+             * @var string $value The keyword string
+             * @var string $type The keyword type from Parser
+             */
+            foreach(self::$keywords as $value => $type) {
+              /** @var int $op_len Length of the operator we are comparing */
               $op_len = strlen($value);
               if(substr_compare($line, $value, $i, $op_len) == 0){
                 $token = new Token($type, $value, $i);
                 array_push($tokens, $token);
                 $i += $op_len - 1;
-                if($value == '{{') {
-                  $search_keywords = true;
-                } else if($value == '}}'){
-                  $search_keywords = false;
-                }
                 break;
               }
             }
-            break;
-          default:
-            if($search_keywords){
-              foreach(self::$keywords as $value => $type) {
-                $op_len = strlen($value);
-                if(substr_compare($line, $value, $i, $op_len) == 0){
-                  $token = new Token($type, $value, $i);
-                  array_push($tokens, $token);
-                  $i += $op_len - 1;
-                  break;
-                }
-              }
-            } else {
+          } else {
 
-            }
-          break;
+          }
         }
       }
       print_r($tokens) . '<br/>';
@@ -105,33 +117,36 @@
     /**
      * Creates a T_NUMERIC token from the input string
      * @access public
-     * @param string the string that will be tokenized
-     * @param int the index at which the number starts in $line
-     * @param int the length of the $line
-     * @return Token the resulting T_NUMERIC token
-     * @throws Exception if the number is not formed correctly, 1..234
+     * @param string $line The string that will be tokenized
+     * @param int $i The index at which the number starts in $line
+     * @param int $len The length of the $line
+     * @return Token The resulting T_NUMERIC token
+     * @throws Exception If the number is not formed correctly, 1..234
      */
     function tokenizeNumeric(string $line, int &$i, int $len): Token {
+      /** @var string $value Holds the full number that will be tokenized */
       $value = $line[$i++];
+
+      /** @var bool $has_dot Used in the case of a float */
       $has_dot = false;
+
+      // Parse string starting from $i to find the full number
       for($j = $i; $j < $len; $j++){
-        switch($line[$j]){
-          case '.':
-            if($has_dot) {
-              throw new Exception('Improperly formed number starting at position ' . $i);
-              break;
-            } else {
-              $has_dot = true;
-              $value .= $line[$j];
-              break;
-            }
-          case '0': case '1': case '2': case '3': case '4':
-          case '5': case '6': case '7': case '8': case '9':
+        if(preg_match(self::$regex[self::RE_NUMERIC], $line[$j])) {
+          // Concat found number to $value
+          $value .= $line[$j];
+        } else if($line[$j] === '.') {
+          if(!$has_dot) {
+            // The number is a float
+            $has_dot = true;
             $value .= $line[$j];
-            break;
-          default:
-            $i = $j;
-            break 2;
+          } else {
+            throw new Exception('Improperly formed number starting at position ' . $i);
+          }
+        } else { // Non-numeric character found, full number is in $value
+          // Update $i to match current offset
+          $i = $j;
+          break;
         }
       }
       return new Token(Parser::T_NUMERIC, $value, $i);
